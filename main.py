c0d44340288866bed82e71883d6e5508b3900009
jdbgambit <jbaugher.dc@gmail.com>
2025-09-16 17:26:55 -0400

    add clock

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     running = True
     while running:
         for event in pygame.event.get():
@@ -14,6 +16,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60)  / 1000.0
 
 
 
